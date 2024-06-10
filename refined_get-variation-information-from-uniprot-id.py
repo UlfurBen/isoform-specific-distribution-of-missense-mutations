b965d34d799838f90a32026a7cdccbb92e08a7f7
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-06-10 11:33:07 +0000

    Update refined_get-variation-information-from-uniprot-id.py

--- refined_get-variation-information-from-uniprot-id.py
+++ refined_get-variation-information-from-uniprot-id.py
@@ -3,7 +3,7 @@ import requests
 import json
 import pandas as pd
 
-# Path to your CSV file
+# Path to your CSV file (fetched from link on epigeneticmachinery.org website)
 csv_file_path = "/Users/ulfurfjolnisson/Downloads/The Epigenetic Machinery.csv"
 
 # Read the CSV file into a pandas DataFrame
@@ -17,9 +17,14 @@ print(UniProt_ids)
 
 # Retrieve variation data from uniprot-ids
 for UniProt_id in UniProt_ids:
-    # Iterate over stripped isoform accessions and fetch data
+    # Iterate over UniProt IDs fetch data
     requestURL = f"https://www.ebi.ac.uk/proteins/api/variation/{UniProt_id}?format=json"
 
     response = requests.get(requestURL)
     data = response.json()
     print(data)
+    
+    file_name = f"{UniProt_id}.json"
+    # Save the data to a file named after UniProt ID
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent = 4)
