dcc86d78c3d20fb27c95dc3870f4dd7b6801d141
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-07-15 10:29:27 +0000

    Update script-optimised.R

--- count-of-isoform-specific-ClinVar-missense-variants/script-optimised.R
+++ count-of-isoform-specific-ClinVar-missense-variants/script-optimised.R
@@ -1,9 +1,9 @@
-# Set the library path
-.libPaths("/hpchome/ubf2/R/x86_64-pc-linux-gnu-library/4.1")
-
 # Load necessary libraries
 library(dplyr)
 library(data.table)
+
+# Set the library path
+.libPaths("/hpchome/ubf2/R/x86_64-pc-linux-gnu-library/4.1")
 
 # Define the input and output file paths
 input_file_regions <- "Homo_sapiens.GRCh38.112.chr_patch_hapl_scaff_isoform_specific_regions.bed"
@@ -14,19 +14,20 @@ output_file <- "Homo_sapiens.GRCh38.112.chr_patch_hapl_scaff_isoform_specific_re
 regions_data <- fread(input_file_regions, header = TRUE, sep = "\t")
 variants_data <- fread(input_file_variants, header = TRUE, sep = "\t")
 
-# Sort the variants data by chromosome and start position
-variants_data <- variants_data[order(variants_data$chr, variants_data$chromStart)]
+# Rename columns for merging
+setnames(variants_data, old = c("chr", "chromStart", "chromEnd"), new = c("chr", "start", "end"))
 
-# Define a function to count the variants in each region
-count_variants_in_region <- function(chr, start, end, variants_data) {
-  subset_variants <- variants_data[chr == variants_data$chr & start <= variants_data$chromEnd & end >= variants_data$chromStart]
-  return(nrow(subset_variants))
+# Create a key for fast joins
+setkey(variants_data, chr, start, end)
+
+# Function to count variants in each region
+count_variants <- function(chr, start, end) {
+  variants_in_region <- variants_data[chr == chr & start <= end & end >= start, .N]
+  return(variants_in_region)
 }
 
 # Apply the function to each row of the regions data
-regions_data <- regions_data %>%
-  rowwise() %>%
-  mutate(variant_count = count_variants_in_region(chr, chromStart, chromEnd, variants_data))
+regions_data[, variant_count := count_variants(chr, chromStart, chromEnd), by = .(chr, chromStart, chromEnd)]
 
 # Write the output to a new BED file
 fwrite(regions_data, output_file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
