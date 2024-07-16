a437fd8efabb20f8d1c5c6f60e4876e516a2524f
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-07-16 13:03:33 +0000

    Update script.R

--- categorical-change/all-genes/find-categorical-change-lp-p/script.R
+++ categorical-change/all-genes/find-categorical-change-lp-p/script.R
@@ -1,15 +1,15 @@
 # Define file paths
-# Get input_file by running grep -i 'pathogenic' homo_sapiens_variation_missense_ClinVar.txt | grep -i 'likely pathogenic' | grep -vi 'uncertain' | grep -vi 'benign' | grep -vi 'likely benign' > homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only.txt
-input_file <- "homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only.txt"
+# Get input_file by running grep -i 'pathogenic' homo_sapiens_variation_missense_ClinVar.txt | grep -i 'likely pathogenic' | grep -vi 'uncertain' | grep -vi 'benign' | grep -vi 'likely benign' > homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only_all_genes.txt
+input_file <- "homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only_all_genes.txt"
 property_lookup_file <- "Amino_acid_change_with_property_change.txt"
-output_file <- "homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only_aa_change.txt"
+output_file <- "homo_sapiens_variation_missense_ClinVar_pathogenic_likely_pathogenic_only_all_genes_aa_change.txt"
 
 # Read the input files
-data <- read.table(pipe(paste('grep -i "Pathogenic" "', input_file, '" | grep -vi "likely benign" | grep -vi "uncertain" | grep -vi "benign" | grep -vi "likely pathogenic"', sep = "")), header = FALSE, sep = "\t", stringsAsFactors = FALSE, quote = "")
+data <- read.table(input_file, header = FALSE, sep = "\t", stringsAsFactors = FALSE, quote = "")
 properties <- read.table(property_lookup_file, header = FALSE, sep = " ", stringsAsFactors = FALSE, quote = "")
 
 # Rename columns for better understanding
-colnames(data) <- c("Column1", "Isoform", "AminoAcidChange", "Column4", "Column5", "Column6", "Column7", "Column8", "Column9", "Column10", "Column11", "Column12", "Column13")
+colnames(data) <- c("Column1", "Isoform", "AminoAcidChange", "Column4", "Column5", "Column6", "Column7", "Column8", "Column9", "Column10", "Column11", "Column12", "Column13", "Column14")
 colnames(properties) <- c("AminoAcidChange", "PropertyChange")
 
 # Function to remove numeric characters between amino acid names
