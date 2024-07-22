3de11bd22608187257286a29acd87c30374edd71
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-07-22 13:38:25 +0000

    Update script.R

--- find-non-overlapping-isoform-regions/ensembl-data-gtf-to-bed/script.R
+++ find-non-overlapping-isoform-regions/ensembl-data-gtf-to-bed/script.R
@@ -1,12 +1,11 @@
 # Load necessary library
 library(dplyr)
-library(tidyr)
 
 # Define output files
 output_bed_file <- "Homo_sapiens.GRCh37.87.bed"
 output_bed_file_with_headers <- "Homo_sapiens.GRCh37.87_with_headers.bed"
 
-# Read the GTF file, skipping lines starting with '#!'
+# Read the GTF file
 gtf_file <- "Homo_sapiens.GRCh37.87.gtf"
 gtf_data <- read.table(gtf_file, sep="\t", header=FALSE, comment.char="#", quote="")
 
@@ -16,21 +15,8 @@ colnames(gtf_data) <- c("chr", "source", "feature", "start", "end", "score", "st
 # Filter the data to include only lines containing "CDS"
 cds_data <- gtf_data %>% filter(feature == "CDS")
 
-# Add additional columns with NA values
-cds_data <- cds_data %>%
-  mutate(
-    gene_id = NA, gene_version = NA, transcript_id = NA, transcript_version = NA,
-    exon_number = NA, gene_name = NA, gene_source = NA, gene_biotype = NA,
-    transcript_name = NA, transcript_source = NA, transcript_biotype = NA,
-    protein_id = NA, protein_version = NA
-  )
-
-# Select relevant columns and rearrange
-bed_df <- cds_data %>%
-  select(chr, start, end, source, feature, score, strand, frame, attribute, 
-         gene_id, gene_version, transcript_id, transcript_version, exon_number, 
-         gene_name, gene_source, gene_biotype, transcript_name, transcript_source, 
-         transcript_biotype, protein_id, protein_version)
+# Swap columns: move 4th and 5th column to 2nd and 3rd positions and vice versa
+bed_df <- cds_data %>% select(chr, start, end, source, feature, score, strand, frame, attribute)
 
 # Write to a BED file
 write.table(bed_df, file=output_bed_file, sep="\t", quote=FALSE, col.names=FALSE, row.names=FALSE)
@@ -39,13 +25,10 @@ write.table(bed_df, file=output_bed_file, sep="\t", quote=FALSE, col.names=FALSE
 bed_df <- read.table(output_bed_file, sep="\t", header=FALSE)
 
 # Set column names
-colnames(bed_df) <- c("chr", "chromStart", "chromEnd", "name", "score", "strand", "frame", "attribute", 
-                      "gene_id", "gene_version", "transcript_id", "transcript_version", "exon_number", 
-                      "gene_name", "gene_source", "gene_biotype", "transcript_name", "transcript_source", 
-                      "transcript_biotype", "protein_id", "protein_version")
+colnames(bed_df) <- c("chr", "chromStart", "chromEnd", "name", "score", "strand", "frame", "attribute", "miscellaneous")
 
 # Display the first 10 rows to confirm changes
 print(head(bed_df, 10))
 
-# Write the modified data frame back to a new file with headers
+# Write the modified data frame back to a new file
 write.table(bed_df, file=output_bed_file_with_headers, sep="\t", quote=FALSE, col.names=TRUE, row.names=FALSE)
