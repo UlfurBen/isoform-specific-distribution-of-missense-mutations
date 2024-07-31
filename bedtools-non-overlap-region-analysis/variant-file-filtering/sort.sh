e6206b7bea121077e3d21377d64444f17236cf4c
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-07-31 14:15:36 +0000

    Update sort.sh

--- bedtools-non-overlap-region-analysis/variant-file-filtering/sort.sh
+++ bedtools-non-overlap-region-analysis/variant-file-filtering/sort.sh
@@ -1,1 +1,1 @@
-sort -k1,1 -k2,2n homo_sapiens_variation_missense_ClinVar_filtered_relevancy_no_headers.bed -o sorted_homo_sapiens_variation_missense_ClinVar_filtered_relevancy_no_headers.bed
+sort -k1,1 -k2,2n homo_sapiens_variation_missense_ClinVar_filtered_relevancy_no_headers_pathogenic.bed -o sorted_homo_sapiens_variation_missense_ClinVar_filtered_relevancy_no_headers_pathogenic.bed
