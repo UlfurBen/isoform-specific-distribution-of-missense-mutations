f4436ad38bc078006790b618396128667c2170c4
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-08-09 12:46:11 +0000

    Update script.R

--- bedtools-non-overlap-region-analysis/gene-folders-with-isoform-specific-variants/make-gene-names-file/script.R
+++ bedtools-non-overlap-region-analysis/gene-folders-with-isoform-specific-variants/make-gene-names-file/script.R
@@ -6,5 +6,3 @@ gene_names <- data$Gene_Name
 
 # Write the gene names to a CSV file without quotation marks and with headers
 write.csv(gene_names, file = "gene_names.csv", row.names = FALSE, quote = FALSE)
-
-# Manually remove first column from gene_names.csv
