e63b9621b82054c3ab52b2b813cd7c30b175fa2b
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-08-12 13:52:31 +0000

    Update script.R

--- bedtools-non-overlap-region-analysis/gene-folders-with-isoform-specific-variants/correspondance-searching/script.R
+++ bedtools-non-overlap-region-analysis/gene-folders-with-isoform-specific-variants/correspondance-searching/script.R
@@ -2,15 +2,15 @@
 library(dplyr)
 
 # Define file paths
-format_results_path <- "~/final-results/format_results.bed"
-benign_format_results_path <- "~/final-results/benign_format_results.bed"
+results_path <- "~/final-results/results.bed"
+benign_results_path <- "~/final-results/benign_results.bed"
 
 # Read the files without headers
-format_results <- read.table(format_results_path, header = FALSE, sep = "\t")
-benign_format_results <- read.table(benign_format_results_path, header = FALSE, sep = "\t")
+results <- read.table(results_path, header = FALSE, sep = "\t")
+benign_results <- read.table(benign_results_path, header = FALSE, sep = "\t")
 
 # Perform the join based on the first 3 columns
-merged_results <- left_join(format_results, benign_format_results[, c(1:3, 6)], 
+merged_results <- left_join(results, benign_results[, c(1:3, 6)], 
                             by = c("V1" = "V1", "V2" = "V2", "V3" = "V3"))
 
 # Write the result to a new file called appended-results.bed
