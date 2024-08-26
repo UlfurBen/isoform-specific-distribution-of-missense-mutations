23d7bb9e098637f79e717078105175b5f9d3cf62
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-08-26 07:53:16 +0000

    Update parallelized-script.R

--- concluding-script/pipeline/7-intersect-non-canonical-isoform-variants-and-save/parallelized-script.R
+++ concluding-script/pipeline/7-intersect-non-canonical-isoform-variants-and-save/parallelized-script.R
@@ -68,8 +68,13 @@ process_subfolder <- function(subfolder) {
 num_cores <- detectCores() - 1  # Use all available cores except one
 cl <- makeCluster(num_cores)
 
+# Load necessary libraries on each worker node
+clusterEvalQ(cl, {
+  library(data.table)
+})
+
 # Export necessary variables and functions to the cluster
-clusterExport(cl, varlist = c("process_folder", "process_subfolder"))
+clusterExport(cl, varlist = c("process_folder", "process_subfolder", "system"))
 
 # Apply the function in parallel to each subfolder
 parLapply(cl, subfolders, process_subfolder)
