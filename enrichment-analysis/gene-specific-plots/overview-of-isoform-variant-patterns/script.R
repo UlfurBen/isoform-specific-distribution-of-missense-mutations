1ffc3ecc53c4b6b51b5603174969a53b5f42a067
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2024-09-13 22:50:36 +0000

    Update script.R

--- enrichment-analysis/gene-specific-plots/overview-of-isoform-variant-patterns/script.R
+++ enrichment-analysis/gene-specific-plots/overview-of-isoform-variant-patterns/script.R
@@ -7,7 +7,7 @@ enrichment_data <- read.csv("enrichment.txt", header = TRUE)
 # Identify main isoform (without hyphen in identifier)
 enrichment_data$Is_Rare_Isoform <- grepl("-", enrichment_data$Identifier)
 
-# Calculate the total number of variants in each category using only 'Variation count'
+# Calculate the total number of variants in each Type using only 'Variation count'
 total_all_variants <- sum(enrichment_data$Variation.count)
 total_main_variants <- sum(enrichment_data$Variation.count[!enrichment_data$Is_Rare_Isoform])
 total_rare_variants <- sum(enrichment_data$Variation.count[enrichment_data$Is_Rare_Isoform])
@@ -19,12 +19,12 @@ percent_rare_isoform <- (total_rare_variants / total_all_variants) * 100
 
 # Prepare data for plotting
 plot_data <- data.frame(
-  Isoform_Category = c("Main Isoform", "All Isoforms", "Rare Isoforms"),
+  Isoform_Type = c("Main Isoform", "All Isoforms", "Rare Isoforms"),
   Percentage = c(percent_main_isoform, percent_all_isoforms, percent_rare_isoform)
 )
 
 # Plot the data
-ggplot(plot_data, aes(x = Isoform_Category, y = Percentage, fill = Isoform_Category)) +
+ggplot(plot_data, aes(x = Isoform_Type, y = Percentage, fill = Isoform_Type)) +
   geom_bar(stat = "identity") +
-  labs(title = "Missense Variant Distribution Across Isoforms", x = "Isoform Category", y = "Percentage of Variants (%)") +
+  labs(title = "Missense Variant Distribution Across Different Isoform Types", x = "Isoform Type", y = "Percentage of Variants (%)") +
   theme_minimal()
